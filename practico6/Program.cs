2b933580e1609a01e72991c69225c08de8336196
NazaretCS <nazaretcampos27@gmail.com>
2023-06-04 21:17:46 -0300

    agregado de la resolucion de la primera parte del ejercicio 3

--- practico6/Program.cs
+++ practico6/Program.cs
@@ -1,29 +1,28 @@
-﻿//  PUNTO 2
-using System.Data;  // para poder usar a DataTable
+﻿//  PUNTO 3
+using System;
+using System.Collections.Generic;
 
-string[] operacionesSimbol = { "+", "-", "*", "/" };
-string respuesta;
+string[] arrayOperaciones = {"Math.Abs(num1)", "Math.Pow(num1, 2)", "Math.Sqrt(numero)", "Math.Sin(angRadianes)", "Math.Cos(angRadianes)" };
 
-do
+// En lugar de almacenar las operaciones en un arreglo como en calculadora1 las almaceno en un diccionario ya que considero que es una estructura de datos mas adecuada
+Dictionary<string, Func<double, double>> operaciones = new Dictionary<string, Func<double, double>>();
+
+//agrego las operaciones al diccionario haciando uso de .Add
+operaciones.Add("Valor Absoluto", Math.Abs);
+operaciones.Add("Cuadrado", x => Math.Pow(x, 2));
+operaciones.Add("Raiz Cuadrada", Math.Sqrt);
+operaciones.Add("Seno", Math.Sin);
+operaciones.Add("Coseno", Math.Cos);
+operaciones.Add("Parte Entera", Math.Floor);
+
+Console.WriteLine("\nPor favor ingrese un numero para operar:");
+double.TryParse(Console.ReadLine(), out double num1);
+Console.WriteLine();  // devo buscar otra froma de dejar los espacios jajajsd
+
+foreach (var operacion in operaciones)
 {
-    Console.WriteLine("\n\nSeleccione la operación a Realizar:");
-    Console.WriteLine("\t1 - Suma \n\t2 - Resta\n\t3 - Multiplicación\n\t4 - Divición\n\t");
-    int.TryParse(Console.ReadLine(), out int operacionRealizar);
-    
-    // Obtengo los numeros a operar...
-    Console.WriteLine("Ingrese el primer valor a operar:");
-    float.TryParse(Console.ReadLine(), out float valor1);
-
-    Console.WriteLine("\nIngrese el segundo valor a operar:");
-    float.TryParse(Console.ReadLine(), out float valor2);
-
-    // formulo la exprecion como string
-    string expresion = $"{valor1} {operacionesSimbol[operacionRealizar - 1]} {valor2}";
-
-    DataTable dt = new DataTable();
-    object resultado = dt.Compute(expresion, ""); // ejecuto la operacion y la almaceno en resultado
-    Console.WriteLine("\nEl resutado es: " + resultado);
-
-    Console.Write("¿Desea realizar otro cálculo?  1 = Si, 0 = No \n");
-    respuesta = Console.ReadLine();
-} while (respuesta == "1");
+    string nombreOperacion = operacion.Key;
+    Func<double, double> funcionOperacion = operacion.Value; 
+    Console.WriteLine($"Operación: {nombreOperacion}");
+    Console.WriteLine($"Resultado: {funcionOperacion(num1)} \n"); 
+}
